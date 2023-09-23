7d5005430db5a53fcea414336f720d835c457353
Maya N <nettathm@gmail.com>
2023-09-23 15:32:54 -0600

    moved server.js outside of the public folder and changed routes on GETs

--- server.js
+++ server.js
@@ -5,17 +5,22 @@ const PORT = 3001; // (BOILERPLATE)
 
 const app = express(); // (BOILERPLATE)
 
+app.use(express.static(path.join(__dirname, "public")));
+
 // GET /notes should return the notes.html file.
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
 // GET * should return the index.html file.
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
+// app.get("/api/notes", (req, res) => {
+//   res.sendFile(path.join(__dirname, "db/db.json"));
+// });
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
